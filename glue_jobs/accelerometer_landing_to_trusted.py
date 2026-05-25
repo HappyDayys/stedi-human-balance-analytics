710c3f608092c2b5a09a8969f1cc936732459288
HappyDayys <154571730+HappyDayys@users.noreply.github.com>
2026-05-25 14:42:21 +0530

    Create accelerometer_landing_to_trusted.py

---
+++ glue_jobs/accelerometer_landing_to_trusted.py
@@ -0,0 +1,51 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
+from awsglue import DynamicFrame
+
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
+"""
+
+# Script generated for node a
+a_node1779688875800 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="accelerometer_landing", transformation_ctx="a_node1779688875800")
+
+# Script generated for node c
+c_node1779688877333 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_trusted", transformation_ctx="c_node1779688877333")
+
+# Script generated for node SQL Query
+SqlQuery0 = '''
+select a.*
+from a 
+join c 
+on a.user = c.email
+
+'''
+SQLQuery_node1779688941911 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"a":a_node1779688875800, "c":c_node1779688877333}, transformation_ctx = "SQLQuery_node1779688941911")
+
+# Script generated for node Amazon S3
+EvaluateDataQuality().process_rows(frame=SQLQuery_node1779688941911, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1779688809838", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+AmazonS3_node1779689194506 = glueContext.getSink(path="s3://stedi-sthiti/accelerometer_trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1779689194506")
+AmazonS3_node1779689194506.setCatalogInfo(catalogDatabase="stedi",catalogTableName="accelerometer_trusted")
+AmazonS3_node1779689194506.setFormat("glueparquet", compression="snappy")
+AmazonS3_node1779689194506.writeFrame(SQLQuery_node1779688941911)
+job.commit()
